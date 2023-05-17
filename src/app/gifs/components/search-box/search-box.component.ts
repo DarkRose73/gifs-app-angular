78500ac8e35b3c2014b5edc6e9e322d5e2fd0fb1
Joan Salas <joan.salas.silva@gmail.com>
2023-05-17 18:00:37 -0400

    decorador ViewChild

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
     <h5>Buscar</h5>
-    <input type="text" class="form-control" placeholder="Buscar gifs..." />
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Buscar gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    />
   `,
 })
-export class SearchBoxComponent {}
+export class SearchBoxComponent {
+  // Hacer referencia a un elemento del template, mediante el id
+  // Sirve para hacer referencia local
+  @ViewChild('txtTagInput')
+  tagInput!: ElementRef<HTMLInputElement>;
+
+  // searchTag(newTag: string) {
+  searchTag() {
+    console.log(this.tagInput.nativeElement.value);
+  }
+}
