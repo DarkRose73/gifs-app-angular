db89bd4946ebdb77f45671f174b76137f72b03e7
Joan Salas <joan.salas.silva@gmail.com>
2023-05-17 13:27:09 -0400

    diseño de gifs components

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar</h5>
+    <input type="text" class="form-control" placeholder="Buscar gifs..." />
+  `,
+})
+export class SearchBoxComponent {}
