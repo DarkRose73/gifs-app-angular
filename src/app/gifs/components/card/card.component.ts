506ef7b599f5128a3a2a5cead96337860c6df11a
Joan Salas <joan.salas.silva@gmail.com>
2023-05-24 15:18:22 -0400

    creacion de card component

---
+++ src/app/gifs/components/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'gifs-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent implements OnInit {
+  ngOnInit(): void {
+    if (!this.gif) throw new Error('La propiedad Gif es requerida');
+  }
+
+  @Input()
+  public gif!: Gif;
+}
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [HomePageComponent, SearchBoxComponent, CardListComponent],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent,
+    CardComponent,
+  ],
   imports: [CommonModule],
   exports: [HomePageComponent],
 })
