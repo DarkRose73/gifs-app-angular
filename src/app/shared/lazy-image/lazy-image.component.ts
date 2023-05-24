9738e889a153413229985d1a6c0b6a0f4e4ddfb5
Joan Salas <joan.salas.silva@gmail.com>
2023-05-24 16:10:22 -0400

    gifs app lista

--- src/app/shared/lazy-image/lazy-image.component.ts
+++ src/app/shared/lazy-image/lazy-image.component.ts
@@ -1,7 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
 })
-export class LazyImageComponent {}
+export class LazyImageComponent implements OnInit {
+  @Input() public url!: string;
+
+  @Input() public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('La propiedad URL es requerida');
+  }
+
+  cargando() {
+    setTimeout(() => {
+      this.hasLoaded = true;
+    }, 1000);
+  }
+}
